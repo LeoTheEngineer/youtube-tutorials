8b459b32bba4e45ac6d1e2c621b4fe46fd3b928a
Leo McMillion <109987611+LeoTheEngineer@users.noreply.github.com>
2024-04-24 13:49:39 +0200

    Create cloud-function-edit-firestore-documents.js

---
+++ cloud-function-edit-firestore-documents.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+exports.newCloudFunction = functions.region('us-central1').runWith({memory: '128MB'}).https.onCall((data, context) => {
+  if (!context.auth || !context.auth.uid) {
+    return;
+  }
+  // Update every user document in firestore, this method also allows you to use the users previous values (inside the loop) with: userDoc.data().{field name}
+  const db = admin.firestore();
+  let userSnapshots = await db.collection('users').get();
+  for (userDoc in userSnapshots.docs) {
+    let userRef = db.collection("users").doc(userDoc.id);
+    userRef.update({
+      // Add the parameters you want to edit here
+    })
+  }
+  
+  // Update, create or delete an single document (assuming you have a string parameter to your cloud function called "userRef"):
+  const db = admin.firestore();
+  let userRef = db.collection("users").doc(data.userRef)
+  
+  // Update
+  userRef.update({
+    // Add field value pairs here
+  })
+  
+  // Delete
+  userRef.delete()
+  
+  // Create (cloud function needs to be async)
+  let userRef = await db.collection("users").add({
+    // Add field value pairs here
+  });
+});
